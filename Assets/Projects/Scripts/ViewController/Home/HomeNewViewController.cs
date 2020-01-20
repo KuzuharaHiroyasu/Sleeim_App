52cf2203233083e3b8535da0977389e93b5acb82
DuyPT <p.duy@apptime.co.jp>
2020-01-20 15:28:27 +0900

    新しいHome画面を作成（Part 3)

--- Assets/Projects/Scripts/ViewController/Home/HomeNewViewController.cs
+++ Assets/Projects/Scripts/ViewController/Home/HomeNewViewController.cs
@@ -28,6 +28,7 @@ public class HomeNewViewController : ViewControllerBase
     //PieChart
     public Color[] pieColors; //Colors of Fumei, Mukokyu, Ibiki, Kaimin
     public Image piePrefab;
+    public PieInfo pieInfo;
     public GameObject circleOuter;
 
     [SerializeField] Sprite batteryIcon_unknown = null;
@@ -219,17 +220,25 @@ public class HomeNewViewController : ViewControllerBase
             ChartInfo chartInfo = CSVManager.convertSleepDataToChartInfo(latestSleepDatas);
             if(chartInfo != null)
             {
-                float[] pieValues = new float[4] { chartInfo.pFumei, chartInfo.pMukokyu, chartInfo.pIbiki, chartInfo.pKaiMin }; //Percents of Fumei, Mukokyu, Ibiki, pKaiMin
-                string[] pieLabels = new string[4] { "不明", "無呼吸", "いびき", "快眠" };
+                double p1 = System.Math.Round((double)(chartInfo.pKaiMin * 100), 1);
+                double p2 = System.Math.Round((double)(chartInfo.pIbiki * 100), 1);
+                double p3 = System.Math.Round((double)(chartInfo.pMukokyu * 100), 1);
+                double p4 = 100 - p1 - p2 - p3;
+                p4 = p4 < 0.1 ? 0 : System.Math.Round(p4, 1);
+
+                //p1 = 15; p2 = 20; p3 = 25; p4 = 40;
+                //p1 = 25; p2 = 25; p3 = 25; p4 = 25;
+                double[] pieValues = new double[4] { p1, p2, p3, p4}; //Percents of pKaiMin, Ibiki, Mukokyu, Fumei
+                string[] pieLabels = new string[4] { "快眠", "いびき", "無呼吸", "不明" };
                 makePieChart(pieValues, pieLabels);
             }
         }
     }
 
-    public void makePieChart(float[] pieValues, string[] pieLabels)
+    public void makePieChart(double[] pieValues, string[] pieLabels)
     {
-        float total = 0f;
-        float numPie = pieValues.Length;
+        double total = 0f;
+        int numPie = pieValues.Length;
         for (int i = 0; i < numPie; i++)
         {
             total += pieValues[i];
@@ -237,17 +246,173 @@ public class HomeNewViewController : ViewControllerBase
 
         if (total > 0 && pieColors.Length >= numPie)
         {
-            float zRotation = -180f;
+            //Rotate image to top middle
+            float zRotation = 180f;
+            float zPieInfoRotation = 180f;
+            
             for (int i = 0; i < numPie; i++)
             {
+                pieInfo.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zPieInfoRotation));
+                pieInfo.hidePieInfo();
+
+                float fillAmount = (float)(pieValues[i] / total);
                 Image image = Instantiate(piePrefab) as Image;
                 image.transform.SetParent(circleOuter.transform, false);
                 image.color = pieColors[i];
-                image.fillAmount = pieValues[i] / total;
+                image.fillAmount = fillAmount;
                 image.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, zRotation));
-                zRotation -= image.fillAmount * 360;
+
+                int[] position = getPositionByPercent(fillAmount * 100);
+                PieInfo p = Instantiate(pieInfo) as PieInfo;
+                p.transform.SetParent(image.transform, false);
+                p.transform.localPosition = new Vector3(position[0], position[1] + 189, 0);
+                if (pieValues[i] > 0)
+                {
+                    p.drawPieInfo(pieLabels[i], pieValues[i]);
+                }
+
+                zPieInfoRotation += fillAmount * 360;
+                zRotation -= fillAmount * 360;
             }
         }
+    }
+
+    public int[] getPositionByPercent(double percent)
+    {
+        var dict = new Dictionary<double, int[]>();
+        dict[0]    = new int[2] { -2, -320 };
+        dict[0.25] = new int[2] { -3, -320 };
+        dict[0.5]  = new int[2] { -4, -320 };
+        dict[1]    = new int[2] { -5, -320 };
+        dict[1.5]  = new int[2] { -7, -320 };
+        dict[2]    = new int[2] { -9, -320 };
+        dict[2.5]  = new int[2] { -10, -320 };
+        dict[3]    = new int[2] { -11, -320 };
+        dict[4]    = new int[2] { -15, -320 };
+        dict[5]    = new int[2] { -19, -320 };
+        dict[6]    = new int[2] { -24, -320 };
+        dict[7]    = new int[2] { -28, -320 };
+        dict[7.5]  = new int[2] { -29, -320 };
+        dict[8]    = new int[2] { -32, -320 };
+        dict[9]    = new int[2] { -35, -320 };
+
+        dict[10]   = new int[2] { -40, -320 };
+        dict[11]   = new int[2] { -43, -320 };
+        dict[12]   = new int[2] { -46, -320 };
+        dict[13]   = new int[2] { -50, -320 };
+        dict[14]   = new int[2] { -55, -315 };
+        dict[15]   = new int[2] { -60, -310 };
+        dict[16]   = new int[2] { -64, -310 };
+        dict[17]   = new int[2] { -68, -305 };
+        dict[18]   = new int[2] { -70, -300 };
+        dict[19]   = new int[2] { -72, -300 };
+
+        dict[20]   = new int[2] { -74, -295 };
+        dict[21]   = new int[2] { -77, -290 };
+        dict[22]   = new int[2] { -78, -288 };
+        dict[23]   = new int[2] { -80, -288 };
+        dict[24]   = new int[2] { -82, -286 };
+        dict[25]   = new int[2] { -86, -283 };
+        dict[26]   = new int[2] { -88, -278 };
+        dict[27]   = new int[2] { -89, -273 };
+        dict[28]   = new int[2] { -89, -271 };
+        dict[29]   = new int[2] { -91, -268 };
+
+        dict[30] = new int[2] { -93, -265 };
+        dict[31] = new int[2] { -95, -263 };
+        dict[32] = new int[2] { -95, -257 };
+        dict[33] = new int[2] { -97, -253 };
+        dict[34] = new int[2] { -97, -251 };
+        dict[35] = new int[2] { -97, -248 };
+        dict[36] = new int[2] { -97, -245 };
+        dict[37] = new int[2] { -99, -243 };
+        dict[38] = new int[2] { -101, -241 };
+        dict[39] = new int[2] { -101, -239 };
+
+        dict[40] = new int[2] { -101, -236 };
+        dict[41] = new int[2] { -103, -233 };
+        dict[42] = new int[2] { -105, -230 };
+        dict[43] = new int[2] { -107, -227 };
+        dict[44] = new int[2] { -109, -224 };
+        dict[45] = new int[2] { -109, -221 };
+        dict[46] = new int[2] { -111, -218 };
+        dict[47] = new int[2] { -112, -215 };
+        dict[48] = new int[2] { -114, -212 };
+        dict[49] = new int[2] { -115, -209 };
+
+        dict[50] = new int[2] { -115, -205 };
+        dict[51] = new int[2] { -118, -200 };
+        dict[52] = new int[2] { -120, -196 };
+        dict[53] = new int[2] { -122, -192 };
+        dict[54] = new int[2] { -122, -188 };
+        
+        dict[55] = new int[2] { -122, -184 };
+        dict[56] = new int[2] { -122, -180 };
+        dict[57] = new int[2] { -122, -176 };
+        dict[58] = new int[2] { -122, -172 };
+        dict[59] = new int[2] { -122, -168 };
+
+        dict[60] = new int[2] { -122, -164 };
+        dict[61] = new int[2] { -122, -160 };
+        dict[62] = new int[2] { -122, -156 };
+        dict[63] = new int[2] { -122, -152 };
+        dict[64] = new int[2] { -122, -148 };
+        dict[65] = new int[2] { -122, -144 };
+        dict[66] = new int[2] { -120, -140 };
+        dict[67] = new int[2] { -118, -136 };
+        dict[68] = new int[2] { -116, -132 };
+        dict[69] = new int[2] { -112, -128 };
+
+        dict[70] = new int[2] { -108, -124 };
+        dict[71] = new int[2] { -106, -120 };
+        dict[72] = new int[2] { -106, -116 };
+        dict[73] = new int[2] { -106, -112 };
+        dict[74] = new int[2] { -104, -108 };
+        dict[75] = new int[2] { -100, -104 };
+        dict[76] = new int[2] { -98, -100 };
+        dict[77] = new int[2] { -96, -96 };
+        dict[78] = new int[2] { -92, -92 };
+        dict[79] = new int[2] { -88, -88 };
+
+        dict[80] = new int[2] { -84, -84 };
+        dict[81] = new int[2] { -80, -80 };
+        dict[82] = new int[2] { -76, -76 };
+        dict[83] = new int[2] { -74, -74 };
+        dict[84] = new int[2] { -72, -72 };
+        dict[85] = new int[2] { -70, -70 };
+        dict[86] = new int[2] { -68, -70 };
+        dict[87] = new int[2] { -66, -69 };
+        dict[88] = new int[2] { -64, -69 };
+        dict[89] = new int[2] { -61, -69 };
+
+        dict[90] = new int[2] { -58, -69 };
+        dict[91] = new int[2] { -54, -69 };
+        dict[92] = new int[2] { -50, -69 };
+        dict[93] = new int[2] { -46, -69 };
+        dict[94] = new int[2] { -42, -69 };
+        dict[95] = new int[2] { -38, -69 };
+        dict[96] = new int[2] { -32, -69 };
+        dict[97] = new int[2] { -26, -69 };
+        dict[98] = new int[2] { -21, -69 };
+        dict[99] = new int[2] { -11, -69 };
+        dict[99.5] = new int[2] { -8, -69 };
+        dict[100] = new int[2] { -0, -69 };
+        
+        //key1 <= percent <= key2
+        double key1 = 0;
+        double key2 = 0;
+        foreach (KeyValuePair<double, int[]> entry in dict)
+        {
+            if(percent <= entry.Key) 
+            {
+                key2 = entry.Key;
+                break;
+            } else {
+                key1 = entry.Key;
+            }
+        }
+
+        return (key2 - percent > percent - key1) ? dict[key1] : dict[key2];
     }
 
     //「ニックネーム」ボタンが押されると呼び出される
---
+++ Assets/Projects/Scripts/ViewController/Home/PieInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PieInfo : MonoBehaviour {
+
+    // Use this for initialization
+    public Text pieLabel;
+    public Text piePercentValue;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void drawPieInfo(string label, double percent)
+    {
+        pieLabel.text = label;
+        piePercentValue.text = percent.ToString() + "%";
+    }
+
+    public void hidePieInfo()
+    {
+        pieLabel.text = "";
+        piePercentValue.text = "";
+    }
+}
