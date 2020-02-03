1a49cc3100ef79ccbd25920a0e61534c10004899
DuyPT <p.duy@apptime.co.jp>
2020-02-03 15:26:05 +0900

    info.plisにBluetooth・LocationのDescriptionを追加、UIApplicationExitsOnSuspendを削除

--- Assets/Editor/XcodeSettingsPostProcesser.cs
+++ Assets/Editor/XcodeSettingsPostProcesser.cs
@@ -21,7 +21,16 @@ public class XcodeSettingsPostProcesser {
 		// ここに記載したKey-ValueがXcodeのinfo.plistに反映されます
 		rootDict.SetString("UIStatusBarStyle", "UIStatusBarStyleLightContent");
 
-		rootDict.SetString("NSBluetoothAlwaysUsageDescription", "別の端末にデータを送信する為にBluetoothを使用します");
-		File.WriteAllText(plistPath, plist.WriteToString());
+        // Remove exit on suspend if it exists
+        string exitsOnSuspendKey = "UIApplicationExitsOnSuspend";
+        if (rootDict.values.ContainsKey(exitsOnSuspendKey))
+        {
+            rootDict.values.Remove(exitsOnSuspendKey);
+        }
+
+        rootDict.SetString("NSBluetoothAlwaysUsageDescription", "別の端末にデータを送信する為にBluetoothを使用します");
+        rootDict.SetString("NSBluetoothPeripheralUsageDescription", "睡眠データを取得するために使用します。");
+        rootDict.SetString("NSLocationWhenInUseUsageDescription", "睡眠データを取得するために使用します。");
+        File.WriteAllText(plistPath, plist.WriteToString());
 	}
 }
