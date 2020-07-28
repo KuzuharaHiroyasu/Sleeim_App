b96b876099976040d3d6733e412b0678bd883f48
DuyPT <p.duy@apptime.co.jp>
2020-07-28 15:37:15 +0900

    お問い合わせ画面の追加で[よくある質問][メールで問い合わせ]を対応

---
+++ Assets/Projects/Scripts/ViewController/Setting/Contact/ContactViewControler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kaimin.Managers;
+
+public class ContactViewControler : ViewControllerBase {
+
+	public override SceneTransitionManager.LoadScene SceneTag
+	{
+		get
+		{
+			return SceneTransitionManager.LoadScene.Contact;
+		}
+	}
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// 戻るボタン押下イベントハンドラ
+	/// </summary>
+	public void OnReturnButtonTap()
+	{
+		SceneTransitionManager.LoadLevel(SceneTransitionManager.LoadScene.Setting);
+	}
+
+	public void OnMailContactButtonTap()
+	{
+		HelpMailLuncher.Lunch();
+	}
+
+	public void OnQAButtonTap()
+	{
+		if(HttpManager.IsInternetAvailable())
+		{
+			Application.OpenURL(HttpManager.HTTP_BASE_URL + "/Welness/legal/terms.html");
+		} else
+		{
+			StartCoroutine(HttpManager.showDialogMessage("インターネット未接続のため、ページが開けません。"));
+		}
+	}
+
+}
