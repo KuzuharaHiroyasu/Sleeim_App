7a8e21b9b25ffbe0a6785689715ddc9cb78e1e3f
DuyPT <p.duy@apptime.co.jp>
2020-08-06 09:27:36 +0900

    円グラフのスライド（Demo)を追加

---
+++ Assets/Projects/Scripts/ViewController/Home/PieChart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Kaimin.Managers;
+using UnityEngine.UI;
+
+public class PieChart : MonoBehaviour {
+
+	public Image piePrefab;
+	public PieInfo pieInfo;
+	public Text sleepTimeText = null; //睡眠時間
+	public Text sleepDateText = null; //睡眠日付 (選択したグラフの日付)
+	public GameObject circleOuter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
